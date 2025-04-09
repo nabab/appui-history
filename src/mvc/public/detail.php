c4c4159102a81e6e89df0390ea91f2a4e8a98391
nabab <thomas.nabet@gmail.com>
2025-04-09 14:19:04 +0200

    Fix on detail

--- src/mvc/public/detail.php
+++ src/mvc/public/detail.php
@@ -2,9 +2,10 @@
 /* @var $ctrl \bbn\Mvc\Controller */
 if ( !empty($ctrl->post['uid']) && !empty($ctrl->post['col']) && !empty($ctrl->post['tst']) ){
   $d = null;//$ctrl->getPluginModel('detail', $ctrl->post);
-  if ( is_null($d) ){
+  if (is_null($d)) {
     $d = $ctrl->getModel($ctrl->post);
   }
+
   $ctrl->setTitle($d['title'] ?? _("History line"));
   $views = ['html' => '', 'js' => '', 'css' => ''];//$ctrl->getPluginViews('detail', $d);
   $ctrl->obj->data = $d;
