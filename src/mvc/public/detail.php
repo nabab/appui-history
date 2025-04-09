11bf99dbc60f42c11b5fa774f23c548284fdb8b3
nabab <thomas.nabet@gmail.com>
2025-04-09 12:51:40 +0200

    Fixon details

--- src/mvc/public/detail.php
+++ src/mvc/public/detail.php
@@ -5,7 +5,6 @@ if ( !empty($ctrl->post['uid']) && !empty($ctrl->post['col']) && !empty($ctrl->p
   if ( is_null($d) ){
     $d = $ctrl->getModel($ctrl->post);
   }
-  bbn\X::ddump($d);
   $ctrl->setTitle($d['title'] ?? _("History line"));
   $views = ['html' => '', 'js' => '', 'css' => ''];//$ctrl->getPluginViews('detail', $d);
   $ctrl->obj->data = $d;
