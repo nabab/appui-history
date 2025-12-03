13dbc37b422062966ea701548bbe2e4d04c2474c
nabab <thomas.nabet@gmail.com>
2025-12-03 09:17:56 +0100

    Added operation prop to data-record component

--- src/mvc/html/record.php
+++ src/mvc/html/record.php
@@ -2,5 +2,6 @@
   <div class="bbn-m bbn-b bbn-vpadding bbn-hlpadding bbn-c"
        bbn-text="operations[source.opr] + ' ' + _('in') + ' ' + source.table + ' ' + _('by user') + ' ' + appui.getUserName(source.usr) + ' ' + _('on the') + ' ' + bbn.fn.fdate(source.tst)"/>
   <appui-database-data-record bbn-if="source.data"
-                              :source="source.data"/>
+                              :source="source.data"
+                              :operation="source.opr"/>
 </div>
